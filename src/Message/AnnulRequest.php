d5e56f705d3811b285294d9b091cfc763194ee03
Erik Storli <erikstorli@users.noreply.github.com>
2019-02-11 11:33:17 +0100

    Omnipay v3 compatibility and removed unecessary var_dump in a test. (#13)
    * Omnipay v3 compatibility and removed unecessary var_dump in a test.
    
    * Adding php_codesniffer
    
    * Remove PHP 5.4 & 5.5 from travis.yml.
    
    * Removing PHP 5.3 and adding 7.2 & 7.3.

--- src/Message/AnnulRequest.php
+++ src/Message/AnnulRequest.php
@@ -31,8 +31,8 @@ class AnnulRequest extends PurchaseRequest
     public function sendData($data)
     {
         $url = $this->getEndpoint().'/Netaxept/Process.aspx?';
-        $httpResponse = $this->httpClient->get($url.http_build_query($data))->send();
+        $httpResponse = $this->httpClient->request('GET', $url.http_build_query($data));
 
-        return $this->response = new Response($this, $httpResponse->xml());
+        return $this->response = new Response($this, simplexml_load_string($httpResponse->getBody()->getContents()));
     }
 }
